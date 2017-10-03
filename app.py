19a7c44e538e1603401b23c91218a558adbc34ec
Taylor Wong <taylorwong@Taylors-MacBook-Pro.local>
2017-10-02 23:20:00 -0400

    created login and welcome page in template folder, modifed app.py to make both pages

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 
+    #printing to help check what requests are going through
+    '''
     print "\n\n\n\n"
-    #print "***DIAG: THIS FLASK OBJ***"
-    #print app
+    print "***DIAG: THIS FLASK OBJ***"
+    print app
     print "***DIAG: REQUEST OBJ"
     print request
     print "***DIAG: REQUEST HEADERS***"
@@ -18,7 +20,20 @@ def hello_world():
     print "***DIAG: REQUEST FORM"
     print "form"
     print request.form
-    return render_template("forms.html")
+    '''
+    return render_template("login.html")
+
+
+@app.route("/welcome")
+def welcome():
+    name = request.args['name']
+    username = request.args['username']
+    password = request.args['password']
+    method = request.method
+
+    return render_template("welcome.html", name=name, username=username, password=password, method=method)
+
+
 
 if __name__ == "__main__":
     app.debug = True
